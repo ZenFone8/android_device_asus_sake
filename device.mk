c0ee1653dcb6f6ba2d0c6b9c08193ba9670da5e2
Alexander Koskovich <akoskovich@pm.me>
2022-02-11 09:06:47 -0700

    Revert "sake: Make A2DP offload library a prebuilt."
     * Try this and lets see if it breaks again, in theory it was
       only broken because of prebuilt audio.
    
    This reverts commit 91464503bafbddc80b8f47d627bbe41da62051c1.
    
    Change-Id: I40570e019f6f92bbf2914936db095861cf788f4f

--- device.mk
+++ device.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    liba2dpoffload \
     libbluetooth_audio_session \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
